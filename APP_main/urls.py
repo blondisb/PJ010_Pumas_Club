1d0117c79973c8c2551f6984ed9a12c7e129f208
blondisb <blondisb@uninorte.edu.co>
2023-01-04 18:53:57 -0500

    Other tabs

--- APP_main/urls.py
+++ APP_main/urls.py
@@ -2,5 +2,8 @@ from django.urls import path, include
 from . import views as vw
 
 urlpatterns = [
-    path('', vw.VW_home, name='URL_home')
+    path('', vw.VW_home, name='URL_home'),
+    path('about/', vw.VW_about, name='URL_about'),
+    path('contact/', vw.VW_contact, name='URL_contact'),
+    path('gallery/', vw.VW_gallery, name='URL_gallery')
 ]
--- APP_main/views.py
+++ APP_main/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def VW_home(request):
     return render(request, 'home.html')
+
+def VW_about(request):
+    return render(request, 'about.html')
+
+def VW_contact(request):
+    return render(request, 'contact.html')
+
+def VW_gallery(request):
+    return render(request, 'gallery.html')
