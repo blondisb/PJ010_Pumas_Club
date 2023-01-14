351656613478103d36239dd20226993c2249fc8c
blondisb <blondisb@uninorte.edu.co>
2023-01-13 19:08:03 -0500

    Read 4 Deploy

--- PJ_Pumas/settings.py
+++ PJ_Pumas/settings.py
@@ -60,6 +60,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -93,10 +94,16 @@ WSGI_APPLICATION = 'PJ_Pumas.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    'default': dj_database_url.config(
+            # Feel free to alter this value to suit your needs.
+            # default='postgresql://postgres:postgres@localhost:5432/mysite',
+            default='sqlite:///db.sqlite3',
+            conn_max_age=600
+            )
 }
 
 
@@ -137,6 +144,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# Following settings only make sense on production and may break development environments.
+if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
+    # in your application directory on Render.
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Turn on WhiteNoise storage backend that takes care of compressing static files
+    # and creating unique names for each version so they can safely be cached forever.
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_ROOT = BASE_DIR / 'media/'
 # MEDIA_ROOT2 = BASE_DIR / 'media/APP_med_news/images/'
 # MEDIA_URL = 'public/'
